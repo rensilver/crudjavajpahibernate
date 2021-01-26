4e9fd78142797b5286a5aa2735452413a401598f
rensilver <ren.oliv87@gmail.com>
2021-01-26 11:24:55 -0300

    Project created

---
+++ src/main/java/com/rensilver/crudjavajpahibernate/CrudjavajpahibernateApplication.java
@@ -0,0 +1,13 @@
+package com.rensilver.crudjavajpahibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudjavajpahibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudjavajpahibernateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rensilver/crudjavajpahibernate/CrudjavajpahibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rensilver.crudjavajpahibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudjavajpahibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

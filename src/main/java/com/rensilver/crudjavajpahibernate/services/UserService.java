d686e7f293116dcc0f9fd05ca5ae560a833a81ef
rensilver <ren.oliv87@gmail.com>
2021-01-28 11:12:46 -0300

    Service layer, component registration

---
+++ src/main/java/com/rensilver/crudjavajpahibernate/services/UserService.java
@@ -0,0 +1,26 @@
+package com.rensilver.crudjavajpahibernate.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rensilver.crudjavajpahibernate.entities.User;
+import com.rensilver.crudjavajpahibernate.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
